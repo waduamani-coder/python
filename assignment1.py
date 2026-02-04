be3aa662cd6304e451c50e47e9970a7a0f308cc8
waduamani-coder <waduamani@gmail.com>
2026-02-04 14:33:32 +0300

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,17 @@
+weight=78
+height=1.75
+exponent= height**2
+BMI=weight / exponent
+print("The BMI is: ", BMI)
+
+# Question2
+length=48
+width=25
+area=length * width
+print("the area is: ",area)
+
+# perimeter
+length=48
+width=25
+perimeter=(length + width) *2
+print("the perimeter is: ",perimeter)
