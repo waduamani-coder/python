f9590e77967a15777190f6ad07406510a62eadbf
AmaniWadu <waduamani@gmail.com>
2026-02-07 12:39:26 +0300

    Add files via upload

---
+++ lesson3a.py
@@ -0,0 +1,35 @@
+# Boolean-This is a data type that evaluates either true or false
+
+isRaining=False
+print(isRaining)
+print(type(isRaining))
+
+paidLoan=True
+print(paidLoan)
+print(type(paidLoan))
+
+# comparison: They are used to compare two or more statements and they usually return a boolean answer
+
+number1= 2
+number2= 5
+
+print("is number1 greater than number2?" , number1> number2)
+print("is number1 less than number2?" , number1< number2)
+print("is number1 greater than or equal tonumber2?" , number1>= number2)
+print("is number1 less than or equal to number2?" , number1<= number2)
+print("is number1  equal to number2?" , number1== number2)
+print("is number1 not equal to  number2?" , number1!=number2)
+
+# Logical operators
+# Logical AND
+# It returns true if and only if the condition/statement evaluates to true
+print((3>1) and (7 > 6))
+
+# Logical or
+# It evaluates to true if one of the statements/conditions is true
+print((3 > 1) or (7 > 6))
+
+# Logical not
+# It is used to negate a statement/condition
+print(not(90 > 70) )
+
