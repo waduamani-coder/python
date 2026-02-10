99b11fe32706fd0d7267e5cf8c5fe5ef9620f0d6
AmaniWadu <waduamani@gmail.com>
2026-02-10 11:18:52 +0300

    Add files via upload

---
+++ lesson4a.py
@@ -0,0 +1,14 @@
+# Nested if statements
+# A nested if statement is an if statement that is contained within another if statement.
+
+age= 21
+weight= 46
+
+
+if age > 15:
+    if weight > 50:
+       print("You can donate blood")
+    else:
+       print("you cannot donate blood because of your weight")
+else:
+     print("you cannot donate blood because of your age")   
---
+++ lesson4d.py
@@ -0,0 +1,31 @@
+# Python while loop.
+# While loop executes a block of code repeatedly as long as a certain condition is true
+"""
+initialization of a variable
+while keyword
+followed by the condition/statement to be evaluated,
+followed by a full colon(:),
+code to be printed out,
+increment/decrement
+"""
+
+number=0
+while number < 5:
+    print("Hello Moses", number)
+    number= number + 1
+
+print("=====================")
+# create a python program that prints the even numbers from 50 to 70 using while loop
+
+number=50
+while number <= 70:
+    number=number +2
+    print(number)
+print("=====================")     
+# below is a decreement example that prints the multiples of 3 starting from 201 to 150
+number=201
+while number >= 150:
+    print(number)
+    number = number - 3
+
+# research on python functions.Both with parameters and without parameters.
