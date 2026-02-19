4fe042c93e6428e8284f2bdba9cd1dda24301bf1
waduamani-coder <waduamani@gmail.com>
2026-02-19 19:16:16 +0300

    first commit

--- lesson6e.py
+++ lesson6e.py
@@ -3,9 +3,27 @@
 
 
 
-try:
-    number=100
-    answer = number/0
-    print("The answer is: ", answer)
-except Exception as e:
-    print("There is an error:", e)    
+# try:
+#     number=100
+#     answer = number/0
+#     print("The answer is: ", answer)
+# except Exception as e:
+#     print("There is an error:", e)    
+
+# try:
+#     age = int(input("Enter your age: "))
+#     print("Next year you will be:", age + 1)
+# except Exception as e:
+#     print("There is an error:", e)
+
+# try:
+#     numbers = [10, 20, 30]
+#     print("The number is:", numbers[6])
+# except Exception as e:
+#     print("There is an error:", e)
+
+# try:
+#     marks = int(input("Enter your marks: ")
+#     print("Your marks are:", marks)
+# except ValueError as e:
+#     print("Invalid input! Please enter a number.")    
